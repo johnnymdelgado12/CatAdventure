778be75ce6d5e22a6e5a571c877d782695c6a509
Johnny Delgado <johnny.m.delgado12@gmail.com>
2022-05-30 17:47:13 -0500

    added init game function

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -10,8 +10,17 @@ public class AdventureGame {
         if(start.equalsIgnoreCase("Y")){
             System.out.println("What is your name?");
             String playerName = playerInput.next();
+
+            gameInit(playerName, playerInput);
         } else {
             System.out.println("Goodbye");
         }
+    }
+
+    public static void gameInit(String playerName, Scanner playerInput){
+        int playerHP= 100;
+        String boardPosition = "A1";
+
+        System.out.println("Welcome to the adventure " + playerName + "!");
     }
 }
